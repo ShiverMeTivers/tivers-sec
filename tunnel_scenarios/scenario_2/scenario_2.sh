9567eab80fa5fecb5c994f6943b80f0deec5df99
Someone <zizicool1@gmail.com>
2021-09-22 19:25:25 +0000

    Updated docker configs to build from from image for speed. Fixed Scenario_2

--- tunnel_scenarios/scenario_2/scenario_2.sh
+++ tunnel_scenarios/scenario_2/scenario_2.sh
@@ -7,6 +7,32 @@ fi
 
 case $1 in
 up)
+	function ip_gen() {
+		ips=()
+		for i in $(seq 0 19)
+		do
+		ips+=( $((($RANDOM % 249) + 4)) )
+		done
+	}
+
+	while true
+
+	ip_gen
+	result=$(echo ${ips[@]} | tr ' ' '\n' | sort -nu | wc -w)
+	do
+	if [ $result -eq 19 ] 
+		then 
+			break  
+		else 
+			ip_gen 
+	fi
+	done
+
+	for i in $(seq 0 19)
+	do
+	eval ip$i=$(echo ${ips[$i]})
+	done
+
 
     	#Randomize SSH pivot ports
 	export P2PORT=$(( ($RANDOM % 65535) + 1000))
@@ -20,42 +46,58 @@ up)
 	export tgt2port=$(( ($RANDOM % 65535) + 1000))
 	export tgt3port=$(( ($RANDOM % 65535) + 1000))
 	export tgt4port=$(( ($RANDOM % 65535) + 1000))
+	export tgt5port=$(( ($RANDOM % 65535) + 1000))
+	export tgt6port=$(( ($RANDOM % 65535) + 1000))
+	export tgt7port=$(( ($RANDOM % 65535) + 1000))
 	
 	#Randomize pivot ips
-	export ip2=$(( ($RANDOM % 249) + 4))
-	export ip3=$(( ($RANDOM % 249) + 4))
-	export ip4=$(( ($RANDOM % 249) + 4))
-	export ip5=$(( ($RANDOM % 249) + 4))
-	export ip6=$(( ($RANDOM % 249) + 4))
+	export ip2=$ip2
+	export ip3=$ip3
+	export ip4=$ip4
+	export ip5=$ip5
+	export ip6=$ip6
 
 	#Randomize target ips
-	export tgt1=$(( ($RANDOM % 249) + 4))
-	export tgt2=$(( ($RANDOM % 249) + 4))
-	export tgt3=$(( ($RANDOM % 249) + 4))
-	export tgt4=$(( ($RANDOM % 249) + 4))
+	export tgt1=$ip7
+	export tgt2=$ip8
+	export tgt3=$ip9
+	export tgt4=$ip10
+	export tgt5=$ip11
+	export tgt6=$ip12
+	export tgt7=$ip13
 	
 	#Rebuild Images with random ports
-	docker-compose build > /dev/null
+	#docker-compose build > /dev/null
 
 	#Flush DOCKER-USER Chains in iptables
 	iptables -F DOCKER-USER
 
 	#Bring up docker
-	docker-compose up -d > /dev/null
+	docker-compose up -d 
 
 	#Create iptables rules to segment the network
-	for i in $(seq 1 5); do
-		iptables -I DOCKER-USER -s 172.16.$i.0/24 -d 172.16.$(expr $i + 1).0/24 -j ACCEPT
-		iptables -I DOCKER-USER -s 172.16.$(expr $i + 1).0/24 -d 172.16.$i.0/24 -j ACCEPT
-	done
+	#for i in $(seq 1 5); do
+	#	iptables -I DOCKER-USER -s 172.16.$i.0/24 -d 172.16.$(expr $i + 1).0/24 -j ACCEPT
+	#	iptables -I DOCKER-USER -s 172.16.$(expr $i + 1).0/24 -d 172.16.$i.0/24 -j ACCEPT
+	#done
 
-	#Block SSH from the local machine to anything but 172.16.1.1 (Localhost)
-	sudo iptables -I OUTPUT -p tcp --dport $P2PORT -s 172.16.2.0/24 -d 172.16.2.0/24 -j DROP
-	sudo iptables -I OUTPUT -p tcp --dport $P3PORT -s 172.16.3.0/24 -d 172.16.3.0/24 -j DROP
-	sudo iptables -I OUTPUT -p tcp --dport $P4PORT -s 172.16.4.0/24 -d 172.16.4.0/24 -j DROP
-	sudo iptables -I OUTPUT -p tcp --dport $P5PORT -s 172.16.5.0/24 -d 172.16.5.0/24 -j DROP
-	sudo iptables -I OUTPUT -p tcp --dport $P6PORT -s 172.16.6.0/24 -d 172.16.6.0/24 -j DROP
-	sudo iptables -I OUTPUT -p tcp --dport 22 -s 172.16.1.1 -d 172.16.1.2 -j ACCEPT
+	#Block Connections to Targets from Pivots outside of thier subnets
+	iptables -I DOCKER-USER -s 172.16.1.2 -d 172.16.2.$ip2 -j ACCEPT
+	iptables -I DOCKER-USER -s 172.16.2.$ip2 -d 172.16.3.$ip3 -j ACCEPT
+	iptables -I DOCKER-USER -s 172.16.3.$ip3 -d 172.16.2.$ip2 -j ACCEPT
+	iptables -I DOCKER-USER -s 172.16.3.$ip3 -d 172.16.4.$ip4 -j ACCEPT
+	iptables -I DOCKER-USER -s 172.16.4.$ip4 -d 172.16.3.$ip3 -j ACCEPT
+	iptables -I DOCKER-USER -s 172.16.4.$ip4 -d 172.16.5.$ip5 -j ACCEPT
+	iptables -I DOCKER-USER -s 172.16.5.$ip5 -d 172.16.4.$ip4 -j ACCEPT
+	iptables -I DOCKER-USER -s 172.16.5.$ip5 -d 172.16.6.$ip6 -j ACCEPT
+	iptables -I DOCKER-USER -s 172.16.6.$ip6 -d 172.16.5.$ip5 -j ACCEPT
+
+	#Block Connections from the local machine to anything but 172.16.1.2 (Pivot 1)
+	sudo iptables -I OUTPUT -s 172.16.2.1 -d 172.16.2.0/24 -j REJECT
+	sudo iptables -I OUTPUT -s 172.16.3.1 -d 172.16.3.0/24 -j REJECT
+	sudo iptables -I OUTPUT -s 172.16.4.1 -d 172.16.4.0/24 -j REJECT
+	sudo iptables -I OUTPUT -s 172.16.5.1 -d 172.16.5.0/24 -j REJECT
+	sudo iptables -I OUTPUT -s 172.16.6.1 -d 172.16.6.0/24 -j REJECT
 
 	#Flush Masqurade rules from iptables POSTROUTING chain so auth.log/wtmp/lastlog output will be correct
 	for i in $(seq 1 6); do
